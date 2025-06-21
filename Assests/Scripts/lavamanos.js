3cff8e28c22705e471b3f13f23d23bd84aa075e3
S&E Labs <87960049+S-E-Labs@users.noreply.github.com>
2025-06-21 16:45:02 -0500

    Delete Assests directory

--- Assests/Scripts/lavamanos.js
+++
@@ -1,4 +0,0 @@
-function volverPanel() {
-    window.location.href = 'inventario.html'; // Cambia si el nombre de tu panel es diferente
-  }
-  
--- Assests/Scripts/lavamanos_2.js
+++
@@ -1,4 +0,0 @@
-function volverPanel() {
-    window.location.href = '../Pages/Empleados/empleados.html'; // Cambia si el nombre de tu panel es diferente
-  }
-  
